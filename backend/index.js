91e6b5e3fa25a25bba03c37a1b1260c27561e0ab
Khushal <khushal.innovatemr@gmail.com>
2025-03-03 18:56:16 +0530

    user based authentication

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,10 @@ const PORT = process.env.PORT||3001
 app.use(cors({
     origin: 'https://emp-rust-three.vercel.app',
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token, x-refresh-token, _id',
     credentials: true
 }));
+
 
 app.use(session({
     secret: 'your_secret_key', 
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const verify = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
         return res.status(401).json({
             error: "Unauthorized: No token provided"
         });
